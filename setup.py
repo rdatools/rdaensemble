06611b2442884b8b2aeddb6faa78f23d15380f7f
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-12-13 09:17:33 -0800

    Published initial package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rdaensemble",
+    version="1.0.0",
+    description="Redistricting ensembles",
+    url="https://github.com/rdatools/rdaensemble",
+    author="alecramsay",
+    author_email="a73cram5ay@gmail.com",
+    license="MIT",
+    packages=[
+        "rdaensemble",
+    ],
+    install_requires=["rdabase", "rdascore"],
+    zip_safe=False,
+)
