3b5a9a59f5aa5a78c03ab41690b2512c8783a72d
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-12-19 08:05:05 -0800

    Add %'s of cut edges & boundary nodes

--- rdaensemble/__init__.py
+++ rdaensemble/__init__.py
@@ -3,6 +3,7 @@
 from .rmfrst import random_map, gen_rmfrst_ensemble
 from .rmfrsp import gen_rmfrsp_ensemble
 from .spanning_tree import random_spanning_tree
+from .compactness import cuts_and_boundaries
 from .score import score_ensemble
 from .notable_maps import id_notable_maps
 from .metautils import shared_metadata, ensemble_metadata, scores_metadata
