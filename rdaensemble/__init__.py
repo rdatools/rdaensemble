7790956c955dd3671381f8126f1aa00d73d14706
alecramsay <4155251+alecramsay@users.noreply.github.com>
2023-12-24 08:16:13 -0800

    All but code review on random_map() rework

--- rdaensemble/__init__.py
+++ rdaensemble/__init__.py
@@ -4,9 +4,9 @@ from .rmfrst import random_map, gen_rmfrst_ensemble
 from .rmfrsp import gen_rmfrsp_ensemble
 from .mcmc import gen_mcmc_ensemble
 from .compactness import cuts_and_boundaries
-from .score import score_ensemble
+from .score import score_ensemble, make_plan
 from .notable_maps import id_notable_maps
 from .metautils import shared_metadata, ensemble_metadata, scores_metadata
-from .utils import plan_from_ensemble
+from .utils import make_plan, plan_from_ensemble
 
 name: str = "rdaensemble"
--- rdaensemble/utils.py
+++ rdaensemble/utils.py
@@ -4,6 +4,17 @@ MISCELLANEOUS UTILITIES
 
 from typing import Any, Dict, List
 
+from rdabase import Assignment
+
+
+def make_plan(assignments: Dict[str, int | str]) -> List[Assignment]:
+    """Convert a dict of geoid: district assignments to a list of Assignments."""
+
+    plan: List[Assignment] = [
+        Assignment(geoid, district) for geoid, district in assignments.items()
+    ]
+    return plan
+
 
 def plan_from_ensemble(
     plan_name: str, ensemble: Dict[str, Any]
