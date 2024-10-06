2597e9af7efa7ac8e228d4b6256ce3f70d284294
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-10-06 14:31:03 -0700

    Updated dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rdaensemble",
-    version="1.7.0",
+    version="1.8.0",
     description="Redistricting ensembles",
     url="https://github.com/rdatools/rdaensemble",
     author="alecramsay",
