a7b3d05ff0b66f057fe5463ba5bd653c6aa68012
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-10-08 21:06:19 -0700

    WIP

--- test/test_score.py
+++ test/test_score.py
@@ -17,166 +17,74 @@ class TestMinorityOpportunity:
     def test_is_defined_opportunity_district(self) -> None:
         """Test the function is_defined_opportunity_district()."""
 
-        # AN OPPORTUNITY DISTRICT, D PREFERRED
-
-        # D's won the district
-        dem_votes: int = 100
-        rep_votes: int = 50
-
-        # The minority group preferred the D candidate
-        group_dem_votes: int = 45
-        group_rep_votes: int = 5
-
-        # The minority group votes for the preferred candidate outnumber the white+other votes for the preferred candidate
-        white_dem_votes: int = 40
-        white_rep_votes: int = 60
-
-        # Test the function
+        # The district is a minority opportunity district, D preferred
         result: bool = is_defined_opportunity_district(
-            dem_votes=dem_votes,
-            rep_votes=rep_votes,
-            group_dem_votes=group_dem_votes,
-            group_rep_votes=group_rep_votes,
-            other_dem_votes=white_dem_votes,
-            other_rep_votes=white_rep_votes,
+            dem_votes=100,
+            rep_votes=50,
+            group_dem_votes=45,
+            group_rep_votes=5,
+            other_dem_votes=40,
+            other_rep_votes=60,
         )
-
-        # The district is a minority opportunity district
         assert result is True
 
-        # AN OPPORTUNITY DISTRICT, R PREFERRED
-
-        # D's won the district
-        dem_votes: int = 50
-        rep_votes: int = 100
-
-        # The minority group preferred the D candidate
-        group_dem_votes: int = 5
-        group_rep_votes: int = 45
-
-        # The minority group votes for the preferred candidate outnumber the white+other votes for the preferred candidate
-        white_dem_votes: int = 60
-        white_rep_votes: int = 40
-
-        # Test the function
+        # The district is a minority opportunity district, R preferred
         result: bool = is_defined_opportunity_district(
-            dem_votes=dem_votes,
-            rep_votes=rep_votes,
-            group_dem_votes=group_dem_votes,
-            group_rep_votes=group_rep_votes,
-            other_dem_votes=white_dem_votes,
-            other_rep_votes=white_rep_votes,
+            dem_votes=50,
+            rep_votes=100,
+            group_dem_votes=5,
+            group_rep_votes=45,
+            other_dem_votes=60,
+            other_rep_votes=40,
         )
-
-        # The district is a minority opportunity district
         assert result is True
 
-        # NOT AN OPPORTUNITY DISTRICT
-
-        # D's DIDN'T win the district
-        dem_votes: int = 70
-        rep_votes: int = 80
-
-        # The minority group preferred the D candidate
-        group_dem_votes: int = 45
-        group_rep_votes: int = 5
-
-        # The minority group votes for the preferred candidate outnumber the white+other votes for the preferred candidate
-        white_dem_votes: int = 25
-        white_rep_votes: int = 75
-
-        # Test the function
-        result: bool = is_defined_opportunity_district(
-            dem_votes=dem_votes,
-            rep_votes=rep_votes,
-            group_dem_votes=group_dem_votes,
-            group_rep_votes=group_rep_votes,
-            other_dem_votes=white_dem_votes,
-            other_rep_votes=white_rep_votes,
-        )
-
         # The district is NOT a minority opportunity district
+        # The minority-preferred candidate (D) DIDN'T win the district
+        result: bool = is_defined_opportunity_district(
+            dem_votes=70,
+            rep_votes=80,
+            group_dem_votes=45,
+            group_rep_votes=5,
+            other_dem_votes=25,
+            other_rep_votes=75,
+        )
         assert result is False
 
-        # NOT AN OPPORTUNITY DISTRICT
-
-        # R's DIDN'T win the district
-        dem_votes: int = 80
-        rep_votes: int = 70
-
-        # The minority group preferred the D candidate
-        group_dem_votes: int = 5
-        group_rep_votes: int = 45
-
-        # The minority group votes for the preferred candidate outnumber the white+other votes for the preferred candidate
-        white_dem_votes: int = 75
-        white_rep_votes: int = 25
-
-        # Test the function
-        result: bool = is_defined_opportunity_district(
-            dem_votes=dem_votes,
-            rep_votes=rep_votes,
-            group_dem_votes=group_dem_votes,
-            group_rep_votes=group_rep_votes,
-            other_dem_votes=white_dem_votes,
-            other_rep_votes=white_rep_votes,
-        )
-
         # The district is NOT a minority opportunity district
+        # The minority-preferred candidate (R) DIDN'T win the district
+        result: bool = is_defined_opportunity_district(
+            dem_votes=80,
+            rep_votes=70,
+            group_dem_votes=5,
+            group_rep_votes=45,
+            other_dem_votes=75,
+            other_rep_votes=25,
+        )
         assert result is False
 
-        # NOT AN OPPORTUNITY DISTRICT
-
-        # D's won the district
-        dem_votes: int = 100
-        rep_votes: int = 50
-
-        # The minority group preferred the D candidate
-        group_dem_votes: int = 40
-        group_rep_votes: int = 10
-
-        # The minority group votes for the preferred candidate DON'T outnumber the white+other votes for the preferred candidate
-        white_dem_votes: int = 60
-        white_rep_votes: int = 40
-
-        # Test the function
-        result: bool = is_defined_opportunity_district(
-            dem_votes=dem_votes,
-            rep_votes=rep_votes,
-            group_dem_votes=group_dem_votes,
-            group_rep_votes=group_rep_votes,
-            other_dem_votes=white_dem_votes,
-            other_rep_votes=white_rep_votes,
-        )
-
         # The district is NOT a minority opportunity district
+        # The minority group votes for the preferred candidate (D) DON'T outnumber the white+other votes for the preferred candidate
+        result: bool = is_defined_opportunity_district(
+            dem_votes=100,
+            rep_votes=50,
+            group_dem_votes=40,
+            group_rep_votes=10,
+            other_dem_votes=60,
+            other_rep_votes=50,
+        )
         assert result is False
 
-        # NOT AN OPPORTUNITY DISTRICT
-
-        # DR's won the district
-        dem_votes: int = 50
-        rep_votes: int = 100
-
-        # The minority group preferred the R candidate
-        group_dem_votes: int = 10
-        group_rep_votes: int = 40
-
-        # The minority group votes for the preferred candidate DON'T outnumber the white+other votes for the preferred candidate
-        white_dem_votes: int = 40
-        white_rep_votes: int = 60
-
-        # Test the function
-        result: bool = is_defined_opportunity_district(
-            dem_votes=dem_votes,
-            rep_votes=rep_votes,
-            group_dem_votes=group_dem_votes,
-            group_rep_votes=group_rep_votes,
-            other_dem_votes=white_dem_votes,
-            other_rep_votes=white_rep_votes,
-        )
-
         # The district is NOT a minority opportunity district
+        # The minority group votes for the preferred candidate (R) DON'T outnumber the white+other votes for the preferred candidate
+        result: bool = is_defined_opportunity_district(
+            dem_votes=50,
+            rep_votes=100,
+            group_dem_votes=10,
+            group_rep_votes=40,
+            other_dem_votes=40,
+            other_rep_votes=60,
+        )
         assert result is False
 
     def test_count_defined_opportunity_districts(self) -> None:
